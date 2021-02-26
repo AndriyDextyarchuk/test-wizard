8f63e8b8218b8eff41a9ccf686548e30a2c10cbb
Andriy Dextyarchuk <andriydextyarchuk@gmail.com>
2021-02-26 12:23:04 +0200

    Created Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import classes from './Header.module.scss'
+import {useSelector} from 'react-redux'
 
 export const Header = () => {
+    const path = useSelector(state => state.path)
+    const steps = [
+        {name: 'contacts', id: 1, path: '/'},
+        {name: 'address', id: 2, path: '/step2'},
+        {name: 'categories', id: 3, path: '/step3'},
+        {name: 'success', id: 4, path: '/step4'},
+    ]
+
     return(
-        <header>
-            Header
+        <header className={classes.header}>
+            <ol className={classes.header__bar}>
+                {steps.map(i =>
+                    <li key={i.id} className={i.path === path ? classes.bar_item && classes.active : classes.bar_item}>
+                        {`${i.name}`} 
+                    </li>
+                )}
+            </ol>
         </header>
         
 
