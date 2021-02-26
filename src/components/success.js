8c5dd102ef2119900824be1a04f8bcc614c76165
Andriy Dextyarchuk <andriydextyarchuk@gmail.com>
2021-02-26 12:16:04 +0200

    Created Success component

--- src/components/success.js
+++ src/components/success.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import classes from './success.module.scss'
+
 
 export const Success = () => {
     return (
-        <div>
-            Success
-        </div>
+        <section className={classes.container}>
+            <h1>Succes</h1>
+            <article className={classes.container}>
+                <h5>Your account was successfuly registrated.</h5>
+                <h5>Please wait for account approval.</h5>
+                <h5>It can take up to 24 hours.</h5>
+            </article>
+            <article className={classes.container}>
+                <p>Have questions?</p>
+                <p>Contact <span>info@site.com</span></p>
+            </article>
+        </section>
     )
 }
